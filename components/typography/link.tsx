3b379de067396346133b2d9a419c2aba674a8c74
Michal Srb <srb@convex.dev>
2023-09-28 16:25:42 -0700

    UI and example functions

--- components/typography/link.tsx
+++ components/typography/link.tsx
@@ -1,20 +1,10 @@
-import NextLink from "next/link";
-import { cn, fr } from "@/lib/utils";
+import { se } from "@/lib/utils";
+import { AnchorHTMLAttributes } from "react";
 
-const Link = fr<
-  React.ElementRef<typeof NextLink>,
-  React.ComponentPropsWithoutRef<typeof NextLink>
->(function Link({ className, ...props }, ref) {
-  return (
-    <NextLink
-      className={cn(
-        "font-medium text-primary underline underline-offset-4 hover:no-underline",
-        className
-      )}
-      ref={ref}
-      {...props}
-    />
-  );
-});
-
-export { Link };
+export const Link = se<
+  HTMLAnchorElement,
+  AnchorHTMLAttributes<HTMLAnchorElement>
+>(
+  "a",
+  "font-medium text-primary underline underline-offset-4 hover:no-underline"
+);
--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -14,6 +14,8 @@ import type {
   FilterApi,
   FunctionReference,
 } from "convex/server";
+import type * as _schema from "../_schema";
+import type * as myFunctions from "../myFunctions";
 
 /**
  * A utility for referencing Convex functions in your app's API.
@@ -23,7 +25,10 @@ import type {
  * const myFunctionReference = api.myModule.myFunction;
  * ```
  */
-declare const fullApi: ApiFromModules<{}>;
+declare const fullApi: ApiFromModules<{
+  _schema: typeof _schema;
+  myFunctions: typeof myFunctions;
+}>;
 export declare const api: FilterApi<
   typeof fullApi,
   FunctionReference<any, "public">
